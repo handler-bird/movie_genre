07bc740c8306834206c2f5ca4a51e14374bbc091
Khang Van <k.van@campus.lmu.de>
2024-04-09 16:04:51 +0200

    connection between Vue frontend and FastAPI backend

---
+++ backend/fastAPI.py
@@ -0,0 +1,23 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# allows cross-origin access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
